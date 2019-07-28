a2459ac51884029206f1d41af9976fe91cd2d88d
vovatran1993 <vovatran1993@gmail.com>
2019-07-28 16:38:46 -0500

    sec group added

---
+++ sec_group.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow SSH"
+  vpc_id      = "${vpc-da3f1bbc}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }

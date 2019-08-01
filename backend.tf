a166992092a0e413156257564113ddb308306a95
vovatran1993 <vovatran1993@gmail.com>
2019-07-31 20:18:37 -0500

    ddfdfdf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         dynamodb_table = "dev_state_lock"
         bucket = "teraform-april-2019-vova"
-        region = "eu-west-1"
+        region = "us-east-1"
         key = "dev.tfstate"
     }
 }

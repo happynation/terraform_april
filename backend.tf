b1b0b96a12991443626d03604423fa6e5d1c7b17
vovatran1993 <vovatran1993@gmail.com>
2019-07-31 20:17:47 -0500

    region fixed

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         dynamodb_table = "dev_state_lock"
         bucket = "teraform-april-2019-vova"
-        region = "us-east-1"
+        region = "eu-west-1"
         key = "dev.tfstate"
     }
 }
